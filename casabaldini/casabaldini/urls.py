f1107a82974e55d84abb0bc6f6d378cd5eeee1a9
carlozanieri <carlo.zanieri@gmail.com>
2026-08-05 21:42:37 +0200

    Versione 4.0

--- casabaldini/casabaldini/urls.py
+++ casabaldini/casabaldini/urls.py
@@ -2,7 +2,7 @@
 URL configuration for casabaldini project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("beb.urls")),
+    path("beb/", include("beb.urls")),
     path("admin/", admin.site.urls),
 ]
