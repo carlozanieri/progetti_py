715edcc4ac75ec2725ea7c6fe183881cc90fce47
carlozanieri <carlo.zanieri@gmail.com>
2026-08-30 00:39:36 +0200

    Versione 8.3

--- casabaldini/casabaldini/settings.py
+++ casabaldini/casabaldini/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
     'www.casabaldini.eu',
     'localhost',
     '127.0.0.1',
+    '*',
 ]
 
 # CSRF Settings
@@ -149,3 +150,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_HSTS_SECONDS = 31536000  # 1 anno
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+else:
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
