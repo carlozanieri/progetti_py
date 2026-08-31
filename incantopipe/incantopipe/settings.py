172492bd158f598e228c5a6acbdfb28b4a6661a8
carlozanieri <carlo.zanieri@gmail.com>
2026-09-01 00:49:18 +0200

    Versione 9.6

--- incantopipe/incantopipe/settings.py
+++ incantopipe/incantopipe/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # STATIC & MEDIA
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'media']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
