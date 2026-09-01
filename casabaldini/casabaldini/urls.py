2908f258084a53730b9c0e61e24d5731a10798c2
carlozanieri <carlo.zanieri@gmail.com>
2026-09-02 00:44:02 +0200

    Versione 9.9

--- casabaldini/casabaldini/urls.py
+++ casabaldini/casabaldini/urls.py
@@ -27,22 +27,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-urlpatterns += [
-    
-    re_path(r'^static/(?P<path>.*)$', serve, {
-        'document_root': settings.STATIC_ROOT,
-    }),
-]
 if not settings.DEBUG:
     urlpatterns += [
         re_path(r'^static/(?P<path>.*)$', serve, {
             'document_root': settings.STATIC_ROOT,
         }),
-        re_path(r'^media/(?P<path>.*)$', serve, {
-            'document_root': settings.MEDIA_ROOT,
-        }),
+        
     ]
 else:
     # In sviluppo, Django serve automaticamente da STATICFILES_DIRS
-    from django.conf.urls.static import static
+    #from django.conf.urls.static import static
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
