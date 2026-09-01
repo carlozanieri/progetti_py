0f6501fa0d8266946988f1c671d9ea5a1946a327
carlozanieri <carlo.zanieri@gmail.com>
2026-09-01 22:56:57 +0200

    Versione 9.9

--- casabaldini/casabaldini/urls.py
+++ casabaldini/casabaldini/urls.py
@@ -38,5 +38,5 @@ urlpatterns += [
 
 # Oppure se vuoi servire anche da STATICFILES_DIRS
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ##urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
