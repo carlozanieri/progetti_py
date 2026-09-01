03f1b93815a1e6af2bfde119ceaf8f890cb739e5
carlozanieri <carlo.zanieri@gmail.com>
2026-09-01 22:50:45 +0200

    Versione 9.9

--- casabaldini/casabaldini/urls.py
+++ casabaldini/casabaldini/urls.py
@@ -16,10 +16,27 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 urlpatterns = [
     path("", include("beb.urls")),
     path("beb/", include("beb.urls")),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    re_path(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+]
+
+# Oppure se vuoi servire anche da STATICFILES_DIRS
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
