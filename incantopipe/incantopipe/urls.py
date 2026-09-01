d16bee060e0cf0d984fd481ef82eddbea62a92f5
carlozanieri <carlo.zanieri@gmail.com>
2026-09-01 15:32:13 +0200

    Versione 9.7

--- incantopipe/incantopipe/urls.py
+++ incantopipe/incantopipe/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('', include('store.urls', namespace='store')),
@@ -13,6 +15,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Servi i file statici SEMPRE (anche in produzione)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    re_path(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+]
+
+# Oppure se vuoi servire anche da STATICFILES_DIRS
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
