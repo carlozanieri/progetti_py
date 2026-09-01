b1b033418c0492656272cee2da49aff029a33d5a
carlozanieri <carlo.zanieri@gmail.com>
2026-09-01 18:41:03 +0200

    Versione 9.8

--- casabaldini/beb/urls.py
+++ casabaldini/beb/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-#app_name = "beb"
+app_name = "beb"
 
 urlpatterns = [
     path("", views.home, name="home"),
