9220a196298a97d6ba7baf4c0b2af4d0eb3e5a36
carlozanieri <carlo.zanieri@gmail.com>
2026-09-01 18:30:53 +0200

    Versione 9.8

--- casabaldini/beb/urls.py
+++ casabaldini/beb/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 #app_name = "beb"
 
 urlpatterns = [
@@ -24,4 +27,12 @@ urlpatterns = [
     path("boot", views.boot, name="boot"),
     path("prenotazioni", views.prenotazioni, name="prenotazioni"),
     path("dovemangiare", views.dovemangiare, name="dovemangiare"),
+]
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    re_path(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
 ]
